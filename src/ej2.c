6894a5fb23c90c3d44bf4a2b5993fcbc153ddd0b
rhm00029 <rhm00029@red.ujaen.es>
2026-02-24 11:22:30 +0100

    ej2 solution

--- src/ej2.c
+++ src/ej2.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 void swap(int *a, int *b) {
-    //
+    int v[2] = { 0, 0 };
+
+    v[0] = *a;
+    v[1] = *b;
+
+    *a = v[1];
+    *b = v[0];
 }
 int main(void) {
-    int x = 1, y = 2;
+    int x = -98, y = 4;
     swap(&x, &y);
     printf("x=%d y=%d\n", x, y); // esperado: x=2 y=1
 }
--- src/ej3.c
+++ src/ej3.c
@@ -1,3 +1,27 @@
-//
-// Created by ruben on 22/02/2026.
-//
+#ifndef STATS_H
+#define STATS_H
+#include <stddef.h>
+int max_int(const int *v, size_t n);
+int min_int(const int *v, size_t n);
+double media_int(const int *v, size_t n);
+/* Normaliza en el propio vector:
+v[i] = (v[i] - media) / desviacion
+Si n==0 o desviacion==0, no hace nada.
+*/
+void normaliza(double *v, size_t n);
+#endif
+
+#include "stats.h"
+#include <math.h>
+int max_int(const int *v, size_t n) {
+    // TODO
+}
+int min_int(const int *v, size_t n) {
+    // TODO
+}
+double media_int(const int *v, size_t n) {
+    // TODO
+}
+void normaliza(double *v, size_t n) {
+    // TODO
+}
